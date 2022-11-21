07b714213175a23ec426e6230acdbc5081ca7a93
Axel Crovetto <axelcrovetto2102@gmail.com>
2022-11-21 22:30:31 +0100

    added employee class and config application.yml

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package net.javaguides.springboot.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository {
+}
--- springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package net.javaguides.springbootbackend;
+package net.javaguides.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
